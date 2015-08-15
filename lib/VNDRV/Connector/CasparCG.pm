9124ddf8bfc38d8b1c4a25e15b25ee60d8472a14
Pavel Tolstov <ray66rus@gmail.com>
2015-08-15 20:37:44 +0300

    added stories processing

--- lib/VNDRV/Connector/CasparCG.pm
+++ lib/VNDRV/Connector/CasparCG.pm
@@ -20,9 +20,8 @@ sub BUILD {
 
 	$self->{db} = DB::CGSchema->connect("dbi:Pg:dbname=$dbname;host=$addr", $user, $pass);
 	$self->{data} = $self->{db}->resultset('Main');
-	my $rundown_row = $self->data->find({issue_id => 0, story_id => 0, block_id => 0}, {key => 'path'});
-	$self->data->create({issue_id => 0, story_id => 0, block_id => 0, last => \'NOW()'})
-		unless $rundown_row;
+	$self->data->delete_all;
+	$self->data->create({issue_id => 0, story_id => 0, block_id => 0, last => \'NOW()'});
 }
 
 sub _process_changes {
@@ -31,28 +30,29 @@ sub _process_changes {
 
 	my $changed_issues = $changes->{issues};
 	for my $i_id (keys %$changed_issues) {
-		$self->_process_issue_change($i_id, $changed_issues->{$i_id});
+		$self->_process_issue_changes($i_id, $changed_issues->{$i_id});
 	}
 }
 
-sub _process_issue_change {
+sub _process_issue_changes {
 	my $self = shift;
 	my $i_id = shift;
 	my $issue_delta = shift;
 
 	eval {
-		if(!$issue_delta or _is_air_flag_removed($issue_delta)) {
+		if(!$issue_delta or _is_issue_air_flag_removed($issue_delta)) {
 			$self->_remove_issue($i_id);
-		} elsif(_is_air_falg_added($issue_delta)) {
-			$self->_add_issue($i_id, $issue_delta);
-		} elsif(_significant_field_changed($issue_delta)) {
+		} elsif(_is_issue_air_falg_added($issue_delta)) {
+			$self->_add_issue($i_id);
+		} elsif(_is_significant_issue_field_changed($issue_delta)) {
 			$self->_update_issue_data($i_id, $issue_delta);
 		}
 	};
-	$self->log->error("NUD0020E Error while updating issue $i_id: $@");
+	$self->log->error("NUD0020E Error while updating issue $i_id: $@")
+		if $@;
 }
 
-sub _is_air_flag_removed {
+sub _is_issue_air_flag_removed {
 	my $issue_data = shift;
 	return (defined($issue_data->{type}) and $issue_data->{type}{new} ne 'active') ? 1 : 0;
 }
@@ -65,7 +65,7 @@ sub _remove_issue {
 	$self->data->find({issue_id => 0, story_id => 0, block_id => 0}, {key => 'path'})->update({last => \'NOW()'});
 }
 
-sub _is_air_falg_added {
+sub _is_issue_air_falg_added {
 	my $issue_data = shift;
 	return (defined($issue_data->{type}) and $issue_data->{type}{new} eq 'active') ? 1 : 0;
 }
@@ -73,15 +73,26 @@ sub _is_air_falg_added {
 sub _add_issue {
 	my $self = shift;
 	my $i_id = shift;
-	my $issue_data = shift;
 
-	my $slug = defined($issue_data->{slug}) ? $issue_data->{slug}{new} : '';
+	my $issue = $self->get_issue({issue => $i_id});
+	my $slug = $issue->{slug};
+	$self->data->find({issue_id => 0, story_id => 0, block_id => 0}, {key => 'path'})->update({last => \'NOW()'});
 	$self->data->update_or_create({issue_id => $i_id, issue_slug => $slug, story_id => 0, block_id => 0, last => \'NOW()'});
+	for my $s_id (keys %{$issue->{stories}}) {
+		my $story = $issue->{stories}{$s_id};
+		next
+			unless $story->{active};
+		$self->_add_story({
+				issue_id => $i_id,
+				story_id => $s_id,
+				issue => $issue
+		});
+	}
 }
 
-sub _significant_field_changed {
+sub _is_significant_issue_field_changed {
 	my $issue_data = shift;
-	return defined($issue_data->{slug}) ? 1 : 0;
+	return (defined($issue_data->{slug}) or defined($issue_data->{stories})) ? 1 : 0;
 }
 
 sub _update_issue_data {
@@ -89,7 +100,108 @@ sub _update_issue_data {
 	my $i_id = shift;
 	my $issue_data = shift;
 
-	$self->data->search({issue_id => $i_id})->update({issue_slug => $issue_data->{slug}{new}, last => \'NOW()'});
+	my $issue = $self->get_issue({issue => $i_id});
+	return
+		unless _is_air_issue($issue);
+
+	$self->data->search({issue_id => $i_id})->update({issue_slug => $issue->{slug}, last => \'NOW()'})
+		if defined($issue_data->{slug});
+
+	my $changed_stories = $issue_data->{stories};
+	if(defined($changed_stories)) {
+		for my $s_id (keys %$changed_stories) {
+			$self->_process_story_changes({
+				issue_id => $i_id,
+				story_id => $s_id,
+				story_delta => $changed_stories->{$s_id},
+				issue => $issue
+			});
+		}
+	}
+}
+
+sub _is_air_issue {
+	my $issue = shift;
+	return $issue->{type} eq 'active' ? 1 : 0;
+}
+
+sub _process_story_changes {
+	my $self = shift;
+	my $params = shift;
+
+	my ($i_id, $s_id, $story_delta, $issue) =
+		($params->{issue_id}, $params->{story_id}, $params->{story_delta}, $params->{issue});
+
+	eval {
+		if(!$story_delta or _is_story_active_flag_just_unset($story_delta)) {
+			$self->_remove_story($i_id, $s_id);
+		} elsif(_is_story_active_flag_just_set($story_delta)) {
+			$self->_add_story($params);
+		} elsif(_is_significant_story_field_changed($story_delta)) {
+			$self->_update_story_data($params);
+		}
+	};
+	$self->log->error("NUM0021E Error while updating story $s_id: $@")
+		if $@;
+}
+
+sub _is_story_active_flag_just_unset {
+	my $story_data = shift;
+	return (defined($story_data->{active}) and !$story_data->{active}{new}) ? 1 : 0;
+}
+
+sub _remove_story {
+	my $self = shift;
+	my $i_id = shift;
+	my $s_id = shift;
+
+	$self->data->search({story_id => $s_id})->delete_all;
+	$self->data->find({issue_id => $i_id, story_id => 0, block_id => 0}, {key => 'path'})->update({last => \'NOW()'});
+}
+
+sub _is_story_active_flag_just_set {
+	my $story_data = shift;
+	return (defined($story_data->{active}) and $story_data->{active}{new}) ? 1 : 0;
+}
+
+sub _add_story {
+	my $self = shift;
+	my $params = shift;
+
+	my ($i_id, $s_id, $issue) =
+		($params->{issue_id}, $params->{story_id}, $params->{issue});
+
+	my $story = $issue->{stories}{$s_id};
+	$self->data->find({issue_id => $i_id, story_id => 0, block_id => 0}, {key => 'path'})->update({last => \'NOW()'});
+	$self->data->update_or_create({
+		issue_id => $i_id,
+		issue_slug => $issue->{slug},
+		story_id => $s_id,
+		story_slug => $story->{slug},
+		block_id => 0,
+		last => \'NOW()'
+	});
+}
+
+sub _is_significant_story_field_changed {
+	my $story_data = shift;
+	return (defined($story_data->{slug}) or defined($story_data->{blocks})) ? 1 : 0;
+}
+
+sub _update_story_data {
+	my $self = shift;
+	my $params = shift;
+
+	my ($i_id, $s_id, $story_delta, $issue) =
+		($params->{issue_id}, $params->{story_id}, $params->{story_delta}, $params->{issue});
+
+	my $story = $issue->{stories}{$s_id};
+	return
+		unless $story->{active};
+
+
+	$self->data->search({issue_id => $i_id, story_id => $s_id})->update({story_slug => $story->{slug}, last => \'NOW()'})
+		if defined($story_delta->{slug});
 }
 
 1;
