0653df043fc0c6207e674a978c75d6ca24451588
Pavel Tolstov <ray66rus@gmail.com>
2015-08-17 20:12:35 +0300

    added compilation of binaries and compiled binary

--- tools/make_packed_executables.sh
+++ tools/make_packed_executables.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 carton exec pp -a lib/ -M Moose -M List::MoreUtils::PP -M Modern::Perl -M SQL::Translator -o bin/deploydb bin/deploydb.pl
+carton exec pp -a lib/ -M Moose -M List::MoreUtils::PP -M Modern::Perl -M SQL::Translator -M Log::Handler::Output::Screen -M LWP::UserAgent -o bin/vndrv bin/vndrv.pl
