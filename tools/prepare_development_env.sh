d8a207fde3a2e0170bc6a039fd9489ef2d5926e9
Pavel Tolstov <ray66rus@gmail.com>
2015-12-23 20:51:25 +0300

    updated development script

--- tools/prepare_development_env.sh
+++ tools/prepare_development_env.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-curl -kL http://install.perlbrew.pl | bash && perlbrew init
+curl -kL http://install.perlbrew.pl | bash
 source ~/perl5/perlbrew/etc/bashrc
+perlbrew init
 if ! grep -q "source ~/perl5/perlbrew/etc/bashrc" ~/.bashrc; then
 	echo "source ~/perl5/perlbrew/etc/bashrc" >> ~/.bashrc
+fi
+if ! grep -q "source ~/perl5/perlbrew/etc/bashrc" ~/.bash_profile; then
+	echo "source ~/perl5/perlbrew/etc/bashrc" >> ~/.bash_profile
 fi
 perlbrew install perl-5.18.2 -Dusethreads --as threaded-perl-5.18.2
 perlbrew lib create threaded-perl-5.18.2@vndrv
 perlbrew use threaded-perl-5.18.2@vndrv
 echo "N" | perlbrew install-cpanm
 cpanm Carton
-carton install
+carton install --deployment
