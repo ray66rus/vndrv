650a316ddd49224e74c4b0b5b3e8d01d550dce84
Pavel Tolstov <ray66rus@gmail.com>
2016-01-15 01:46:29 +0300

    work on new table ('afisha')

---
+++ lib/DB/BarcoLNSPBSchema/Result/Afisha.pm
@@ -0,0 +1,69 @@
+package DB::BarcoLNSPBSchema::Result::Afisha;
+
+use Modern::Perl;
+use base qw/DBIx::Class::Core/;
+
+# Associated table in database
+__PACKAGE__->table('afisha');
+
+# Column definition
+my %columns = (
+	id => {
+		data_type => 'integer',
+		is_auto_increment => 1,
+	},
+	time => {
+		data_type => 'timestamp'
+	},
+	issue_id => {
+		data_type => 'bigint'
+	},
+	story_id => {
+		data_type => 'bigint'
+	},
+	block_id => {
+		data_type => 'bigint'
+	},
+	caption_id => {
+		data_type => 'varchar(32)',
+	},
+	header => {
+		data_type => 'text',
+		is_nullable => 1,
+	},
+	day => {
+		data_type => 'text',
+		is_nullable => 1,
+	},
+	time => {
+		data_type => 'text',
+		is_nullable => 1,
+	},
+	color => {
+		data_type => 'text',
+		is_nullable => 1,
+	},
+);
+for(my $i=1;$i<=3;$i++) {
+	$columns{"subheader$i"} = { data_type => 'text', is_nullable => 1 };
+	$columns{"foto$i"} = { data_type => 'text', is_nullable => 1 };
+	$columns{"place$i"} = { data_type => 'text', is_nullable => 1 }
+		if $i < 3;
+}
+
+__PACKAGE__->add_columns(%columns);
+
+# Tell DBIC  that 'id' is the primary key
+__PACKAGE__->set_primary_key('id');
+__PACKAGE__->add_unique_constraint('path' => [qw/issue_id story_id block_id caption_id/]);
+
+sub sqlt_deploy_hook {
+	my ($self, $sqlt_table) = @_;
+	$sqlt_table->add_index(name => 'issue_id_idx', fields => ['issue_id']);
+	$sqlt_table->add_index(name => 'story_id_idx', fields => ['story_id']);
+	$sqlt_table->add_index(name => 'block_id_idx', fields => ['block_id']);
+	$sqlt_table->add_index(name => 'caption_id_idx', fields => ['caption_id']);
+	$sqlt_table->options({'CHARACTER SET' => 'UTF8', COLLATE => 'utf8_general_ci'});
+}
+
+1;
--- migrations/cg/DB-OradCGSchema-1-MySQL.sql
+++ migrations/cg/DB-OradCGSchema-1-MySQL.sql
@@ -1,6 +1,6 @@
 -- 
 -- Created by SQL::Translator::Producer::MySQL
--- Created on Wed Sep 23 01:32:50 2015
+-- Created on Fri Jan 15 01:44:37 2016
 -- 
 SET foreign_key_checks=0;
 
@@ -14,18 +14,19 @@ CREATE TABLE news_data (
   last timestamp NOT NULL,
   captions text NULL,
   issue_id bigint NOT NULL,
-  issue_slug text NULL,
+  issue_name text NULL,
+  issue_start datetime NULL,
   story_id bigint NOT NULL,
-  story_slug text NULL,
+  story_name text NULL,
   block_id bigint NOT NULL,
-  block_slug text NULL,
+  block_name text NULL,
   INDEX issue_id_idx (issue_id),
   INDEX story_id_idx (story_id),
   INDEX block_id_idx (block_id),
   INDEX timestamp_idx (last),
   PRIMARY KEY (id),
   UNIQUE path (issue_id, story_id, block_id)
-) DEFAULT CHARACTER SET UTF-8;
+) COLLATE utf8_general_ci;
 
 SET foreign_key_checks=1;
 
